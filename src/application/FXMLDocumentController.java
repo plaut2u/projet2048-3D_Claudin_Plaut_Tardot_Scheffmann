3fa9a740fa738f89ca3340641b6541533543fb19
plaut2u <plaut2u@PLG-IDMC-C03-10.ad.univ-lorraine.fr>
2019-09-25 17:07:32 +0200

    I add things

--- src/application/FXMLDocumentController.java
+++ src/application/FXMLDocumentController.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package XMLpackage;
+package application;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +12,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -22,14 +25,18 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
+    public ImageView img16;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package XMLpackage;
+package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +24,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        
         
         stage.setScene(scene);
         stage.show();
